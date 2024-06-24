9d1b649fe70bf8fa4894266085f2b6c40ffff919
HarshalAtre <113234343+HarshalAtre@users.noreply.github.com>
2024-06-25 03:13:24 +0530

    Update config.js

--- NeuralChat-Server/constants/config.js
+++ NeuralChat-Server/constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
     origin: [
       "http://localhost:5173",
       "http://localhost:4173",
-        "https://neural-chat-nad4.vercel.app/",
+        "https://neural-chat-nad4.vercel.app",
       process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
